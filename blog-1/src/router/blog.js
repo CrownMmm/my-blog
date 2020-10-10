b44398ca6a73c87cbdb54c83e262159df0c910a3
zhangjiaming <823600537@qq.com>
2020-10-10 17:33:02 +0800

    feat：添加blog user路由

--- blog-1/src/router/blog.js
+++ blog-1/src/router/blog.js
@@ -1,113 +1,43 @@
-const {
-    getList,
-    getDetail,
-    newBlog,
-    updateBlog,
-    delBlog
-} = require('../controller/blog')
-const { SuccessModel, ErrorModel } = require('../model/resModel')
-
-// 统一的登录验证函数
-const loginCheck = (req) => {
-    if (!req.session.username) {
-        return Promise.resolve(
-            new ErrorModel('尚未登录')
-        )
-    }
-}
-
 const handleBlogRouter = (req, res) => {
-    const method = req.method // GET POST
-    const id = req.query.id
+  const method = req.method; //get post
+  const url = req.url;
+  const path = url.split("?")[0];
 
-    // 获取博客列表
-    if (method === 'GET' && req.path === '/api/blog/list') {
-        let author = req.query.author || ''
-        const keyword = req.query.keyword || ''
-        // const listData = getList(author, keyword)
-        // return new SuccessModel(listData)
+  //  获取博客列表
+  if (method === "GET" && path === "api/blog/list") {
+    return {
+      msg: "这是获取博客列表的接口",
+    };
+  }
 
-        if (req.query.isadmin) {
-            // 管理员界面
-            const loginCheckResult = loginCheck(req)
-            if (loginCheckResult) {
-                // 未登录
-                return loginCheckResult
-            }
-            // 强制查询自己的博客
-            author = req.session.username
-        }
+  //获取博客详情
+  if (method === "GET" && path === "api/blog/detail") {
+    return {
+      msg: "这是获取博客详情的接口",
+    };
+  }
 
-        const result = getList(author, keyword)
-        return result.then(listData => {
-            return new SuccessModel(listData)
-        })
-    }
+  //新建博客
+  if (method === "POST" && path === "api/blog/new") {
+    return {
+      msg: "这是新建博客的接口",
+    };
+  }
 
-    // 获取博客详情
-    if (method === 'GET' && req.path === '/api/blog/detail') {
-        // const data = getDetail(id)
-        // return new SuccessModel(data)
-        const result = getDetail(id)
-        return result.then(data => {
-            return new SuccessModel(data)
-        })
-    }
+  //更新博客
+  if (method === "POST" && path === "api/blog/update") {
+    return {
+      msg: "这是更新博客的接口",
+    };
+  }
 
-    // 新建一篇博客
-    if (method === 'POST' && req.path === '/api/blog/new') {
-        // const data = newBlog(req.body)
-        // return new SuccessModel(data)
+  //删除博客
+  if (method === "POST" && path === "api/blog/del") {
+    return {
+      msg: "这是删除博客的接口",
+    };
+  }
+};
 
-        const loginCheckResult = loginCheck(req)
-        if (loginCheckResult) {
-            // 未登录
-            return loginCheckResult
-        }
-
-        req.body.author = req.session.username
-        const result = newBlog(req.body)
-        return result.then(data => {
-            return new SuccessModel(data)
-        })
-    }
-
-    // 更新一篇博客
-    if (method === 'POST' && req.path === '/api/blog/update') {
-        const loginCheckResult = loginCheck(req)
-        if (loginCheckResult) {
-            // 未登录
-            return loginCheckResult
-        }
-
-        const result = updateBlog(id, req.body)
-        return result.then(val => {
-            if (val) {
-                return new SuccessModel()
-            } else {
-                return new ErrorModel('更新博客失败')
-            }
-        })
-    }
-
-    // 删除一篇博客
-    if (method === 'POST' && req.path === '/api/blog/del') {
-        const loginCheckResult = loginCheck(req)
-        if (loginCheckResult) {
-            // 未登录
-            return loginCheckResult
-        }
-
-        const author = req.session.username
-        const result = delBlog(id, author)
-        return result.then(val => {
-            if (val) {
-                return new SuccessModel()
-            } else {
-                return new ErrorModel('删除博客失败')
-            }
-        })
-    }
-}
-
-module.exports = handleBlogRouter
+module.exports = handleBlogRouter;
+ 
--- blog-1/src/router/user.js
+++ blog-1/src/router/user.js
@@ -1,42 +1,15 @@
-const { login } = require('../controller/user')
-const { SuccessModel, ErrorModel } = require('../model/resModel')
-const { set } = require('../db/redis')
-
 const handleUserRouter = (req, res) => {
-    const method = req.method // GET POST
+  const method = req.method; //get post
+  const url = req.url;
+  const path = url.split("?")[0];
 
-    // 登录
-    if (method === 'POST' && req.path === '/api/user/login') {
-        const { username, password } = req.body
-        // const { username, password } = req.query
-        const result = login(username, password)
-        return result.then(data => {
-            if (data.username) {
-                // 设置 session
-                req.session.username = data.username
-                req.session.realname = data.realname
-                // 同步到 redis
-                set(req.sessionId, req.session)
+  //登录
+  if (method === "POST" && path === "api/user/login") {
+    return {
+      msg: "这是登录的接口",
+    };
+  }
+};
 
-                return new SuccessModel()
-            }
-            return new ErrorModel('登录失败')
-        })
-    }
-
-    // // 登录验证的测试
-    // if (method === 'GET' && req.path === '/api/user/login-test') {
-    //     if (req.session.username) {
-    //         return Promise.resolve(
-    //             new SuccessModel({
-    //                 session: req.session
-    //             })
-    //         )
-    //     }
-    //     return Promise.resolve(
-    //         new ErrorModel('尚未登录')
-    //     )
-    // }
-}
-
-module.exports = handleUserRouter
+module.exports = handleUserRouter;
+ 
