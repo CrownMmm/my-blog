ed614aa0f0eac4539df9d62ad607e1fb91148548
zhangjiaming <823600537@qq.com>
2020-11-16 00:04:00 +0800

    拷贝路由及其controller

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -2,9 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/list', function (req, res, next) {
-  res.json({
-    errno: 0,
-    data: [1, 2, 3]
+  let author = req.query.author || ''
+  const keyword = req.query.keyword || ''
+
+  if (req.query.isadmin) {
+    console.log('is admin')
+    // 管理员界面
+    if (req.session.username == null) {
+      console.error('is admin, but no login')
+      // 未登录
+      res.json(
+        new ErrorModel('未登录')
+      )
+      return
+    }
+    // 强制查询自己的博客
+    author = req.session.username
+  }
+
+  const result = getList(author, keyword)
+  return result.then(listData => {
+    res.json(
+      new SuccessModel(listData)
+    )
   })
 });
 
